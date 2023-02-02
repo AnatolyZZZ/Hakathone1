d552695b3e6c83c9dcc8a0470caca3e0a5e85c73
Ekaterina <katya.leemi@gmail.com>
2023-02-02 11:53:52 +0200

    Added check of zone color

--- script.js
+++ script.js
@@ -1,45 +1,72 @@
 const root = document.getElementById('root')
+const error = document.createElement('p')
+let boxesId = 0
 
 const onDragStart = (e) => {
     console.log(e.target.id)
+    error.style.display = 'none'
     e.dataTransfer.setData("text/plain", e.target.id)
 }
 
 const onDragOver = (e) => {
     e.preventDefault()
-    console.log(e.dataTransfer)
 }
 
 const onDrop = (e) => {
     e.preventDefault()
     let data = e.dataTransfer.getData("text/plain");
-    console.log(data)
+    // checking if target is correct zone
+    // console.log(e.eventPhase)
+    // console.log(e.target)
+    // console.log(e.currentTarget)
+    
+    let parent = e.currentTarget
     let child = document.getElementById(data)
-    child.setAttribute('draggable', false)
-    child.classList.add('dropped')
-    e.target.appendChild(child)
+    if (parent.dataset.zoneColor === child.dataset.boxColor) {
+        child.setAttribute('draggable', false)
+        child.classList.add('dropped')
+        parent.appendChild(child)
+    } else {
+        error.style.display = 'block'
+    }
 }
 
-const generateBoxes = () => {
+const generateBoxes = (color) => {
     for (let i = 0; i < 2; i++) {
         const box = document.createElement('div')
         box.classList.add('box')
-        box.setAttribute('id', i)
+        box.style.background = color
+        box.setAttribute('id', boxesId)
+        box.setAttribute('data-box-color', color)
+        boxesId++
         box.setAttribute('draggable', true)
-        
+
         box.addEventListener('dragstart', onDragStart)
-        
+
         root.append(box)
     }
 }
 
-const generateZones = () => {
+const generateZones = (color) => {
     const zone = document.createElement('div')
+    zone.setAttribute('data-zone-color', color)
     zone.classList.add('zone')
+    zone.style.background = color
     zone.addEventListener('dragover', onDragOver)
-    zone.addEventListener('drop', onDrop)
+
+    // boolean true stops bubbling which prevents appending box to another box in the zone
+    zone.addEventListener('drop', onDrop, true)
     root.append(zone)
 }
 
-generateBoxes()
-generateZones()
+const createError = () => {
+    error.classList.add('error')
+    error.append('Wrooong!')
+    root.append(error)
+}
+
+generateBoxes('red')
+generateBoxes('green')
+generateZones('red')
+generateZones('green')
+createError()
